b91665e722851908e31c812b34cf4484d9146c28
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-05 20:46:41 +0400

    Change createCategoryContainer function to add data-* attributes to job entity elements.

--- load_data.js
+++ load_data.js
@@ -89,7 +89,7 @@ function createDescriptionContainer(entity){
   return container;
 }
 
-function createCategoryContainer(entity){
+function createCategoryContainer(entity, root){
   const container = document.createElement("div");
   container.setAttribute("class", "category-container");
 
@@ -99,22 +99,49 @@ function createCategoryContainer(entity){
   const tools = entity.tools;
 
   const roleElement = document.createElement("span");
+  roleElement.setAttribute("data-category-type", "role");
+  roleElement.setAttribute("data-category-value", role);
+  root.setAttribute("data-role", role);
   roleElement.innerText = role;
   roleElement.setAttribute("class", "role-element");
   container.append(roleElement);
 
   const levelElement = document.createElement("span");
+  levelElement.setAttribute("data-category-type", "level");
+  levelElement.setAttribute("data-category-value", level);
+  root.setAttribute("data-level", level);
   levelElement.innerText = level;
   container.append(levelElement);
 
+  root.setAttribute("data-languages", "");
   for (let language of languages){
     const langElement = document.createElement("span");
+    langElement.setAttribute("data-category-type", "languages");
+    langElement.setAttribute("data-category-value", language);
+
+    const values = root.getAttribute("data-languages");    
+    if (values === ""){
+      root.setAttribute("data-languages", language)
+    } else {
+      root.setAttribute("data-languages", `${values},${language}`);
+    }
     langElement.innerText = language;
     container.append(langElement);
   }
   
+  root.setAttribute("data-tools", "");
   for (let tool of tools){
     const toolElement = document.createElement("span");
+    toolElement.setAttribute("data-category-type", "tools");
+    toolElement.setAttribute("data-category-value", tool);
+
+    const values = root.getAttribute("data-tools");
+    if (values === ""){
+      root.setAttribute("data-tools", tool)
+    } else {
+      root.setAttribute("data-tools", `${values},${tool}`);
+    }
+
     toolElement.innerText = tool;
     container.append(toolElement);
   }
@@ -137,7 +164,8 @@ function createElement(entity){
   const contentContainer = document.createElement("div");
   contentContainer.setAttribute("class", "content-container");
   const descriptionContainer = createDescriptionContainer(entity);
-  const categoryContainer = createCategoryContainer(entity);
+  const categoryContainer = createCategoryContainer(entity, container);
+  categoryContainer.addEventListener("click", updateFilter);
 
   contentContainer.append(descriptionContainer);
   contentContainer.append(categoryContainer);
