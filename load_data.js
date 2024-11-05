04589752fccf80f0ae18a28d4af7a3bf1ba233f4
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-05 20:47:52 +0400

    Add filtering logic.

--- load_data.js
+++ load_data.js
@@ -176,6 +176,133 @@ function createElement(entity){
   return container;
 }
 
+function removeFilter(event){
+
+  event.stopImmediatePropagation();
+  const parent = event.target.closest(".filter-element");
+
+  const categoryType = parent.getAttribute("data-category-type");
+  const categoryValue = parent.getAttribute("data-category-value");
+
+  const filterContainer = document.getElementById("filter-container");
+  const filterElementContainer = document.getElementById("filter-element-container");
+  let filterValue = filterContainer.getAttribute(`data-${categoryType}`);
+
+  const updatedValue = filterValue.replace(categoryValue, "").replace(",,", "");
+  filterContainer.setAttribute(`data-${categoryType}`, updatedValue);
+  
+  parent.remove();
+  if (filterElementContainer.children.length === 0){
+    filterContainer.style.display = "none";
+  }
+  filterJobList();
+}
+
+function createFilterElement(categoryType, categoryValue){
+
+  const element = document.createElement("div");
+  element.setAttribute("class", "filter-element");
+
+  const textContainer = document.createElement("span");
+  textContainer.innerText = categoryValue;
+
+  const iconContainer = document.createElement("div");
+  iconContainer.setAttribute("class", "filter-element-remove");
+  iconContainer.addEventListener("click", removeFilter);
+  const icon = document.createElement("img");
+  icon.setAttribute("src", "./images/icon-remove.svg");
+  iconContainer.append(icon);
+
+  element.append(textContainer);
+  element.append(iconContainer);
+
+  element.setAttribute("data-category-type", categoryType);
+  element.setAttribute("data-category-value", categoryValue);
+
+  return element;
+}
+
+function isSubsetOf(a, b){
+  return a.length === 0 || a.every(value => b.includes(value));
+}
+
+function splitStringNormalized(str){
+  return str === "" ? [] : str.split(",");
+}
+
+function isMatching(child, container){
+
+  childRoles = child.getAttribute("data-role");
+  childRoles = splitStringNormalized(childRoles);
+  childLevels = child.getAttribute("data-level");
+  childLevels = splitStringNormalized(childLevels);
+  childLanguages = child.getAttribute("data-languages")
+  childLanguages = splitStringNormalized(childLanguages);
+  childTools = child.getAttribute("data-tools");
+  childTools = splitStringNormalized(childTools);
+
+  containerRoles = container.getAttribute("data-role");
+  containerRoles = splitStringNormalized(containerRoles);
+  containerLevels = container.getAttribute("data-level");
+  containerLevels = splitStringNormalized(containerLevels);
+  containerLanguages = container.getAttribute("data-languages");
+  containerLanguages = splitStringNormalized(containerLanguages);
+  containerTools = container.getAttribute("data-tools");
+  containerTools = splitStringNormalized(containerTools);
+
+  const roleMatch = isSubsetOf(containerRoles, childRoles);
+  const levelMatch = isSubsetOf(containerLevels, childLevels);
+  const languagesMatch = isSubsetOf(containerLanguages, childLanguages);
+  const toolsMatch = isSubsetOf(containerTools, childTools);
+
+  return roleMatch && levelMatch && languagesMatch && toolsMatch;
+}
+
+function filterJobList(){
+
+  const container = document.getElementById("job-list-container");
+  const filterContainer = document.getElementById("filter-container");
+  
+  for (let i = 1; i < container.children.length; i++){
+    const child = container.children[i];
+
+    if (!isMatching(child, filterContainer)) {
+      child.style.display = "none";
+    } else {
+      child.style.display = "flex";
+    }
+  }
+}
+
+function updateFilter(event){
+
+  if (event.target.tagName != "SPAN"){
+    return;
+  }
+  
+  const target = event.target;
+  const categoryType = target.dataset.categoryType;
+  const categoryValue = target.dataset.categoryValue;
+
+  const filterContainer = document.getElementById("filter-container");
+  filterContainer.style.display = "flex";
+  const filterContainerCategoryValue = filterContainer.getAttribute(`data-${categoryType}`);
+
+  if (filterContainerCategoryValue.includes(categoryValue)){
+    return;
+  }
+  
+  if (filterContainerCategoryValue === ""){
+    filterContainer.setAttribute(`data-${categoryType}`, categoryValue);
+  } else {
+    filterContainer.setAttribute(`data-${categoryType}`, `${filterContainerCategoryValue},${categoryValue}`);
+  }
+  const filterElementContainer = document.getElementById("filter-element-container");
+  const filterElement = createFilterElement(categoryType, categoryValue);
+  filterElementContainer.append(filterElement);
+  filterJobList();
+}
+
 async function insertData(){
 
   const data = await loadData();
