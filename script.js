bcc5499e6921054e29a1faa702ebd573054ee735
Kamo Sevoyan <kamo@Kamo-Sevoyan-MBP.local>
2024-11-06 02:31:46 +0400

    Refactor the code, use global state.

--- script.js
+++ script.js
@@ -1,16 +1,163 @@
+const STATE = {}
+
+async function initializeState(){
+
+  STATE.filters = {
+    role: new Set([]),
+    level: new Set([]),
+    languages: new Set([]),
+    tools: new Set([])
+  }
+
+  const data = await loadData();
+  STATE.data = data;
+
+  const joblistContainer = document.getElementById("job-list-container");
+
+  for (let entity of data) {
+    const element = createElement(entity);
+    joblistContainer.append(element);
+  }
+
+}
+
 async function loadData() {
+
   try {
-    const res = await fetch("./data.json");
-    if (!res.ok) {
+    const result = await fetch("./data.json");
+    if (!result.ok) {
       throw new Error("Failed to load file");
     }
-    return await res.json();
+    return await result.json();
   } catch (error) {
-    return alert(error);
+    return console.log(error);
   }
 }
 
-function createOtherInfoElement(entity){
+function addFilter(event) {
+
+  if (event.target.tagName != "SPAN") {
+    return;
+  }
+
+  const target = event.target;
+  const categoryType = target.dataset.categoryType;
+  const categoryValue = target.dataset.categoryValue;
+
+  STATE.filters[categoryType].add(categoryValue);
+  updateLayout();
+}
+
+function removeFilter(event) {
+
+  event.stopImmediatePropagation();
+  const target = event.target.closest(".filter-element");
+  const categoryType = target.dataset.categoryType;
+  const categoryValue = target.dataset.categoryValue;
+  
+  STATE.filters[categoryType].delete(categoryValue);
+
+  updateLayout();
+}
+
+function clearAllFilters(event) {
+  Object.values(STATE.filters).forEach((x) => x.clear());
+  updateLayout();
+}
+
+function matchFilters(entity){
+  for (const category in STATE.filters){
+    for (const type of STATE.filters[category]){
+      if (!entity[category].includes(type)){
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+function eraseOldLayout(){
+  const joblistContainer = document.getElementById("job-list-container");
+  
+  while (joblistContainer.firstChild) {
+    joblistContainer.removeChild(joblistContainer.firstChild);
+  }
+  const filterContainer = document.getElementById("filter-container");
+  filterContainer?.remove();
+
+}
+
+function updateLayout(){
+
+  eraseOldLayout();
+
+  const joblistContainer = document.getElementById("job-list-container");
+
+  for (const entity of STATE.data){
+    if (matchFilters(entity)){
+      const element = createElement(entity);
+      joblistContainer.append(element);
+    }
+  }
+  if (Object.values(STATE.filters).some((category) => category.size > 0)){
+    createFilterContainer();
+  }
+}
+
+function createFilterContainer(){
+
+  const filterContainer = document.createElement("div");
+  filterContainer.setAttribute("id", "filter-container");
+  const joblistContainer = document.getElementById("job-list-container");
+  joblistContainer.append(filterContainer);
+
+
+  const filterElementContainer = document.createElement("div");
+  filterElementContainer.setAttribute("id", "filter-element-container");
+
+  const clearElement = document.createElement("div");
+  clearElement.innerText = "Clear";
+  clearElement.setAttribute("id", "filter-clear");
+  clearElement.addEventListener("click", clearAllFilters);
+
+  filterContainer.append(filterElementContainer);
+  filterContainer.append(clearElement);
+  
+
+  for (const categoryType in STATE.filters){
+    for (const categoryValue of STATE.filters[categoryType]){
+      const filterElement = createFilterElement(categoryType, categoryValue);
+      filterElementContainer.append(filterElement);
+    }
+  }
+}
+
+function createFilterElement(categoryType, categoryValue) {
+
+  const element = document.createElement("div");
+  element.setAttribute("class", "filter-element");
+
+  const textContainer = document.createElement("span");
+  textContainer.innerText = categoryValue;
+
+  const iconContainer = document.createElement("div");
+  iconContainer.setAttribute("class", "filter-element-remove");
+  iconContainer.addEventListener("click", removeFilter);
+  const icon = document.createElement("img");
+  icon.setAttribute("src", "./assets/icons/icon-remove.svg");
+  iconContainer.append(icon);
+
+  element.append(textContainer);
+  element.append(iconContainer);
+
+  element.setAttribute("data-category-type", categoryType);
+  element.setAttribute("data-category-value", categoryValue);
+
+  return element;
+}
+
+function createOtherInfoElement(entity) {
 
   const container = document.createElement("div");
   container.setAttribute("class", "other-info");
@@ -30,11 +177,12 @@ function createOtherInfoElement(entity){
   container.append(postedAtElement);
   container.append(contractTypeElement);
   container.append(locationElement);
-  
+
   return container;
 }
 
-function createCompanyElement(entity){
+function createCompanyElement(entity) {
+
   const container = document.createElement("div");
   container.setAttribute("class", "company-description")
 
@@ -51,14 +199,14 @@ function createCompanyElement(entity){
   const indicatorContainer = document.createElement("div");
   indicatorContainer.setAttribute("class", "new-featured");
 
-  if (isNew){
+  if (isNew) {
     const isNewElement = document.createElement("span");
     isNewElement.innerText = "NEW!";
     isNewElement.setAttribute("class", "new-indicator");
     indicatorContainer.append(isNewElement);
   }
 
-  if (isFeatured){
+  if (isFeatured) {
     const isFeaturedElement = document.createElement("span");
     isFeaturedElement.innerText = "FEATURED";
     isFeaturedElement.setAttribute("class", "featured-indicator");
@@ -66,11 +214,11 @@ function createCompanyElement(entity){
   }
 
   container.append(indicatorContainer);
-  return container; 
+  return container;
 }
 
-function createDescriptionContainer(entity){
-  
+function createDescriptionContainer(entity) {
+
   const container = document.createElement("div");
   container.setAttribute("class", "job-description");
 
@@ -81,7 +229,7 @@ function createDescriptionContainer(entity){
 
   const otherInfoElement = createOtherInfoElement(entity);
   const companyNameElement = createCompanyElement(entity);
-  
+
   container.append(companyNameElement);
   container.append(positionElement);
   container.append(otherInfoElement);
@@ -89,7 +237,8 @@ function createDescriptionContainer(entity){
   return container;
 }
 
-function createCategoryContainer(entity, root){
+function createCategoryContainer(entity, root) {
+
   const container = document.createElement("div");
   container.setAttribute("class", "category-container");
 
@@ -114,13 +263,13 @@ function createCategoryContainer(entity, root){
   container.append(levelElement);
 
   root.setAttribute("data-languages", "");
-  for (let language of languages){
+  for (let language of languages) {
     const langElement = document.createElement("span");
     langElement.setAttribute("data-category-type", "languages");
     langElement.setAttribute("data-category-value", language);
 
-    const values = root.getAttribute("data-languages");    
-    if (values === ""){
+    const values = root.getAttribute("data-languages");
+    if (values === "") {
       root.setAttribute("data-languages", language)
     } else {
       root.setAttribute("data-languages", `${values},${language}`);
@@ -128,15 +277,15 @@ function createCategoryContainer(entity, root){
     langElement.innerText = language;
     container.append(langElement);
   }
-  
+
   root.setAttribute("data-tools", "");
-  for (let tool of tools){
+  for (let tool of tools) {
     const toolElement = document.createElement("span");
     toolElement.setAttribute("data-category-type", "tools");
     toolElement.setAttribute("data-category-value", tool);
 
     const values = root.getAttribute("data-tools");
-    if (values === ""){
+    if (values === "") {
       root.setAttribute("data-tools", tool)
     } else {
       root.setAttribute("data-tools", `${values},${tool}`);
@@ -149,7 +298,8 @@ function createCategoryContainer(entity, root){
   return container;
 }
 
-function createElement(entity){
+function createElement(entity) {
+
   const container = document.createElement("div");
 
   container.setAttribute("class", "job-entity");
@@ -157,7 +307,7 @@ function createElement(entity){
 
   const image = document.createElement("img");
   image.setAttribute("src", entity.logo);
-  image.setAttribute("alt", "ALT"); //TODO: change later
+  image.setAttribute("alt", "Company logo");
 
   container.append(image);
 
@@ -165,7 +315,7 @@ function createElement(entity){
   contentContainer.setAttribute("class", "content-container");
   const descriptionContainer = createDescriptionContainer(entity);
   const categoryContainer = createCategoryContainer(entity, container);
-  categoryContainer.addEventListener("click", updateFilter);
+  categoryContainer.addEventListener("click", addFilter);
 
   contentContainer.append(descriptionContainer);
   contentContainer.append(categoryContainer);
@@ -174,162 +324,4 @@ function createElement(entity){
   container.setAttribute("data-is-new", entity.new);
 
   return container;
-}
-
-function removeFilter(event){
-
-  event.stopImmediatePropagation();
-  const parent = event.target.closest(".filter-element");
-
-  const categoryType = parent.getAttribute("data-category-type");
-  const categoryValue = parent.getAttribute("data-category-value");
-
-  const filterContainer = document.getElementById("filter-container");
-  const filterElementContainer = document.getElementById("filter-element-container");
-  let filterValue = filterContainer.getAttribute(`data-${categoryType}`);
-
-  const updatedValue = filterValue.replace(categoryValue, "").replace(",,", "");
-  filterContainer.setAttribute(`data-${categoryType}`, updatedValue);
-  
-  parent.remove();
-  if (filterElementContainer.children.length === 0){
-    filterContainer.style.display = "none";
-  }
-  filterJobList();
-}
-
-function createFilterElement(categoryType, categoryValue){
-
-  const element = document.createElement("div");
-  element.setAttribute("class", "filter-element");
-
-  const textContainer = document.createElement("span");
-  textContainer.innerText = categoryValue;
-
-  const iconContainer = document.createElement("div");
-  iconContainer.setAttribute("class", "filter-element-remove");
-  iconContainer.addEventListener("click", removeFilter);
-  const icon = document.createElement("img");
-  icon.setAttribute("src", "./assets/icons/icon-remove.svg");
-  iconContainer.append(icon);
-
-  element.append(textContainer);
-  element.append(iconContainer);
-
-  element.setAttribute("data-category-type", categoryType);
-  element.setAttribute("data-category-value", categoryValue);
-
-  return element;
-}
-
-function isSubsetOf(a, b){
-  return a.length === 0 || a.every(value => b.includes(value));
-}
-
-function splitStringNormalized(str){
-  return str === "" ? [] : str.split(",");
-}
-
-function isMatching(child, container){
-
-  childRoles = child.getAttribute("data-role");
-  childRoles = splitStringNormalized(childRoles);
-  childLevels = child.getAttribute("data-level");
-  childLevels = splitStringNormalized(childLevels);
-  childLanguages = child.getAttribute("data-languages")
-  childLanguages = splitStringNormalized(childLanguages);
-  childTools = child.getAttribute("data-tools");
-  childTools = splitStringNormalized(childTools);
-
-  containerRoles = container.getAttribute("data-role");
-  containerRoles = splitStringNormalized(containerRoles);
-  containerLevels = container.getAttribute("data-level");
-  containerLevels = splitStringNormalized(containerLevels);
-  containerLanguages = container.getAttribute("data-languages");
-  containerLanguages = splitStringNormalized(containerLanguages);
-  containerTools = container.getAttribute("data-tools");
-  containerTools = splitStringNormalized(containerTools);
-
-  const roleMatch = isSubsetOf(containerRoles, childRoles);
-  const levelMatch = isSubsetOf(containerLevels, childLevels);
-  const languagesMatch = isSubsetOf(containerLanguages, childLanguages);
-  const toolsMatch = isSubsetOf(containerTools, childTools);
-
-  return roleMatch && levelMatch && languagesMatch && toolsMatch;
-}
-
-function filterJobList(){
-
-  const container = document.getElementById("job-list-container");
-  const filterContainer = document.getElementById("filter-container");
-  
-  for (let i = 1; i < container.children.length; i++){
-    const child = container.children[i];
-
-    if (!isMatching(child, filterContainer)) {
-      child.style.display = "none";
-    } else {
-      child.style.display = "flex";
-    }
-  }
-}
-
-function updateFilter(event){
-
-  if (event.target.tagName != "SPAN"){
-    return;
-  }
-  
-  const target = event.target;
-  const categoryType = target.dataset.categoryType;
-  const categoryValue = target.dataset.categoryValue;
-
-  const filterContainer = document.getElementById("filter-container");
-  filterContainer.style.display = "flex";
-  const filterContainerCategoryValue = filterContainer.getAttribute(`data-${categoryType}`);
-
-  if (filterContainerCategoryValue.includes(categoryValue)){
-    return;
-  }
-  
-  if (filterContainerCategoryValue === ""){
-    filterContainer.setAttribute(`data-${categoryType}`, categoryValue);
-  } else {
-    filterContainer.setAttribute(`data-${categoryType}`, `${filterContainerCategoryValue},${categoryValue}`);
-  }
-  const filterElementContainer = document.getElementById("filter-element-container");
-  const filterElement = createFilterElement(categoryType, categoryValue);
-  filterElementContainer.append(filterElement);
-  filterJobList();
-}
-
-function clearAllFilters(event) {
-
-  const filterContainer = document.getElementById("filter-container");
-  filterContainer.setAttribute("data-role", "");
-  filterContainer.setAttribute("data-level", "");
-  filterContainer.setAttribute("data-languages", "");
-  filterContainer.setAttribute("data-tools", "");
-
-  const filterElementContainer = document.getElementById("filter-element-container");
-
-  while (filterElementContainer.firstChild) {
-    filterElementContainer.removeChild(filterElementContainer.firstChild);
-  }
-
-  filterContainer.style.display = "none";
-  filterJobList();
-}
-
-async function insertData(){
-
-  const data = await loadData();
-  const flexContainer = document.getElementById("job-list-container");
-  const clearElement = document.getElementById("filter-clear");
-  clearElement.addEventListener("click", clearAllFilters)
-
-  for (let entity of data){
-    const element = createElement(entity);
-    flexContainer.append(element);
-  }
 }
